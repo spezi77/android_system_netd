3ac536a175628675c707890a3a2453abc110cd3e
Thomas Lehner <justremotephone@gmail.com>
2014-11-28 23:13:20 +0100

    NETD: Add option NO_ADDRESSFAMELY_INET to remove IP4 from modifyIpRule.
    Fix for not working WIFI for BRAVO on LOLLIPOP. sendNetlinkRequest
    returns: address family not supported for AF_INET.
    
    Change-Id: Id22a7777199a410664de1def3b7df23184b27c1c

--- server/Android.mk
+++ server/Android.mk
@@ -83,6 +83,10 @@ ifdef WPA_SUPPLICANT_VERSION
   LOCAL_C_INCLUDES += external/wpa_supplicant_8/src/common
 endif
 
+ifeq ($(TARGET_NO_NETD_AF_INET),true)
+    LOCAL_CFLAGS += -DNO_ADDRESSFAMELY_INET
+endif
+
 include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
